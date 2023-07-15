a6a384a8097b50facc2c9998dcd0a019cf744028
eunsung <dmsqls6789@naver.com>
2023-07-15 19:14:10 +0900

    코드 스니펫 실습

--- ScrollView/ScrollView/ScrollView/ViewController.swift
+++ ScrollView/ScrollView/ScrollView/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        showAlert()
     }
 
 
@@ -23,26 +22,25 @@ class ViewController: UIViewController {
         label.text! += "안녕하세요"
     }
     
+    @IBAction func onSubmit(_ sender: Any) {
+        showAlert()
+    }
+
+    
     func showAlert() {
-        // 1. UIAlertController 인스턴스 생성
-        let alertController = UIAlertController(title: "알림", message: "이것은 Alert 예제입니다.", preferredStyle: .alert)
+        let alertController = UIAlertController(title: "알림", message: "알림창 예시", preferredStyle: .alert)
         
-        // 2. 확인 버튼 추가
         let okAction = UIAlertAction(title: "확인", style: .default) { (_) in
-            // 확인 버튼을 눌렀을 때 처리할 내용을 여기에 작성합니다.
             print("확인 버튼이 눌렸습니다.")
         }
         alertController.addAction(okAction)
         
-        // 3. 취소 버튼 추가
         let cancelAction = UIAlertAction(title: "취소", style: .cancel) { (_) in
-            // 취소 버튼을 눌렀을 때 처리할 내용을 여기에 작성합니다.
             print("취소 버튼이 눌렸습니다.")
         }
         alertController.addAction(cancelAction)
         
-        // 4. Alert를 화면에 표시
-        UIApplication.shared.keyWindow?.rootViewController?.present(alertController, animated: true, completion: nil)
+        UIApplication.shared.keyWindow?.rootViewController?.present(alertController, animated: <#bool#>, completion: nil)
     }
 }
 
