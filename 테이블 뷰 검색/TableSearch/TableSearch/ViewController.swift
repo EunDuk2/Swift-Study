5d1ce505a6e4ce850f0733e238ac893e0d16b1e7
eunsung <dmsqls6789@naver.com>
2023-07-17 17:46:50 +0900

    UISearchController적용

--- 테이블 뷰 검색/TableSearch/TableSearch/ViewController.swift
+++ 테이블 뷰 검색/TableSearch/TableSearch/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  TableSearch
-//
-//  Created by EUNSUNG on 2023/07/15.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -21,79 +14,69 @@ class ViewController: UIViewController {
         "경희대학교",
         "한국외국어대학교"
     ]
-    var searchMode:Bool = false
-    var currentText: String = ""
     
-    @IBOutlet var textField: UITextField!
     @IBOutlet var table: UITableView!
+    var searchController: UISearchController!
+    var filteredUni: [String] = ["서울대학교",
+                                 "연세대학교",
+                                 "고려대학교",
+                                 "한양대학교",
+                                 "성균관대학교",
+                                 "서강대학교",
+                                 "이화여자대학교",
+                                 "중앙대학교",
+                                 "경희대학교",
+                                 "한국외국어대학교"] // 검색 결과를 담을 배열
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        textField.delegate = self
+        searchControllerSetting()
     }
-
-
+    
+    func searchControllerSetting() {
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
+    }
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    func searchedCount() -> Int {
-        var count:Int = 0
-        
-        for i in 0..<uni.count {
-            if(searchedIndex(text: currentText, uniIndex: i)) {
-                count += 1
-            }
-        }
-        
-        return count
-    }
-    
-    func searchedIndex(text:String, uniIndex:Int) -> Bool {
-        let universityName = uni[uniIndex]
-        
-        return universityName.hasPrefix(text)
-    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if(searchMode == true) {
-            return searchedCount()
+        if searchController.isActive {
+            return filteredUni.count // 검색 중이면 검색 결과를 반환
         } else {
-            return uni.count
+            return uni.count // 그렇지 않으면 전체 데이터 반환
         }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell") as! TableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
         
-        if(searchMode == true) {
-            if(searchedIndex(text: currentText, uniIndex: indexPath.row)) {
-                cell.label.text = uni[indexPath.row]
-            }
+        if searchController.isActive {
+            
+            cell.label.text = filteredUni[indexPath.row] // 검색 중이면 검색 결과를 표시
         } else {
-            cell.label.text = uni[indexPath.row]
+            cell.label.text = uni[indexPath.row] // 그렇지 않으면 전체 데이터 표시
         }
         
         return cell
     }
 }
 
-extension ViewController: UITextFieldDelegate {
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        // 변경된 텍스트를 감지
-        currentText = (textField.text as NSString?)?.replacingCharacters(in: range, with: string) ?? ""
-        
-        if currentText.isEmpty {
-            searchMode = false
-        } else {
-            searchMode = true
+extension ViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text {
+            filterDataWithSearchText(searchText)
+            table.reloadData()
         }
-        
-        // 텍스트가 변경될 때마다 테이블 뷰를 리로드하여 검색 모드를 반영합니다.
-        table.reloadData()
-        
-        return true
+    }
+    
+    func filterDataWithSearchText(_ searchText: String) {
+        // 검색어가 포함된 학교만 필터링하여 filteredUni에 저장
+        filteredUni = uni.filter { $0.contains(searchText) }
     }
 }
 
