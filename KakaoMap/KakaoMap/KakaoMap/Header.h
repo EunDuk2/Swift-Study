29a53196e67bc58566a43f4538e8925bf67cd93b
eunsung <dmsqls6789@naver.com>
2023-07-18 19:43:38 +0900

    기본 지도 띄우기

--- KakaoMap/KakaoMap/KakaoMap/Header.h
+++ KakaoMap/KakaoMap/KakaoMap/Header.h
@@ -2,12 +2,12 @@
 //  Header.h
 //  KakaoMap
 //
-//  Created by EUNSUNG on 2023/07/17.
+//  Created by EUNSUNG on 2023/07/18.
 //
 
 #ifndef Header_h
 #define Header_h
 
-#import<DaumMap/MTMapView.h>
+#import <DaumMap/MTMapView.h>
 
 #endif /* Header_h */
--- KakaoMap/KakaoMap/KakaoMap/ViewController.swift
+++ KakaoMap/KakaoMap/KakaoMap/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MTMapViewDelegate {
     
-    
+    var mapView:MTMapView!
+    var location1 = MTMapPointGeo(latitude: 37.555351847837, longitude: 126.93557855711)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        mapView = MTMapView(frame: self.view.frame)
+        
+        mapView.delegate = self
+        mapView.baseMapType = .standard
+        self.view.addSubview(mapView)
+        
+        let poiItem1 = MTMapPOIItem()
+        
+        poiItem1.itemName = "회사"
+        poiItem1.mapPoint = MTMapPoint(geoCoord: MTMapPointGeo(latitude: 37.555351847837, longitude: 126.93557855711))
+        poiItem1.markerType = .redPin
+        
+        let poiItem2 = MTMapPOIItem()
+        
+        poiItem2.itemName = "우리집"
+        poiItem2.mapPoint = MTMapPoint(geoCoord: MTMapPointGeo(latitude: 37.481126679565, longitude: 126.94470156213))
+        poiItem2.markerType = .redPin
+        
+        mapView.addPOIItems([poiItem1])
+        mapView.addPOIItems([poiItem2])
     }
 
 
