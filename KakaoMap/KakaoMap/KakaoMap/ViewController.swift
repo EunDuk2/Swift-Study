3ebba308d332636b17b96693afb363b6b63f91e8
eunsung <dmsqls6789@naver.com>
2023-07-18 22:51:51 +0900

    마커 찍기 성공

--- KakaoMap/KakaoMap/KakaoMap/ViewController.swift
+++ KakaoMap/KakaoMap/KakaoMap/ViewController.swift
@@ -10,34 +10,26 @@ import UIKit
 class ViewController: UIViewController, MTMapViewDelegate {
     
     var mapView:MTMapView!
-    var location1 = MTMapPointGeo(latitude: 37.555351847837, longitude: 126.93557855711)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        mapView = MTMapView(frame: self.view.frame)
-        
+        mapView = MTMapView(frame: self.view.bounds)
         mapView.delegate = self
         mapView.baseMapType = .standard
         self.view.addSubview(mapView)
         
         let poiItem1 = MTMapPOIItem()
         
-        poiItem1.itemName = "회사"
-        poiItem1.mapPoint = MTMapPoint(geoCoord: MTMapPointGeo(latitude: 37.555351847837, longitude: 126.93557855711))
+        poiItem1.itemName = "롯데월드"
+        poiItem1.mapPoint = MTMapPoint(geoCoord: MTMapPointGeo(latitude: 37.511545, longitude: 127.098609))
         poiItem1.markerType = .redPin
         
-        let poiItem2 = MTMapPOIItem()
-        
-        poiItem2.itemName = "우리집"
-        poiItem2.mapPoint = MTMapPoint(geoCoord: MTMapPointGeo(latitude: 37.481126679565, longitude: 126.94470156213))
-        poiItem2.markerType = .redPin
-        
         mapView.addPOIItems([poiItem1])
-        mapView.addPOIItems([poiItem2])
+        
+        //mapView.fitAreaToShowAllPOIItems()
     }
-
 
 }
 
