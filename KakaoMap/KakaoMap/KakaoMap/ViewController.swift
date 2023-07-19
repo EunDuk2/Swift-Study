d04bb96dd3922e18df7dc59bd4ef1744f9f09353
eunsung <dmsqls6789@naver.com>
2023-07-19 17:45:18 +0900

    핀말풍선 커스텀해서 동작 수행

--- KakaoMap/KakaoMap/KakaoMap/ViewController.swift
+++ KakaoMap/KakaoMap/KakaoMap/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by EUNSUNG on 2023/07/17.
 //
 // 내 위치로 가는 기능
+// 핀 버튼
 // 주소 적으면 위도, 경도로 바꿔서 핀 찍어주는 기능
 import UIKit
 
@@ -20,7 +21,7 @@ class ViewController: UIViewController {
         setupMapView()
         createPin(itemName: "롯데월드", mapPoint: MTMapPoint(geoCoord: MTMapPointGeo(latitude: 37.511545, longitude: 127.098609)), markerType: .redPin)
         
-        //mapView.fitAreaToShowAllPOIItems()
+        mapView.fitAreaToShowAllPOIItems()
     }
     
     @IBAction func onTrackMyLocation(_ sender: Any) {
@@ -37,19 +38,38 @@ extension ViewController: MTMapViewDelegate {
         mapSubView.addSubview(mapView)
     }
     // 핀 생성
-    func createPin(itemName:String, mapPoint:MTMapPoint, markerType:MTMapPOIItemMarkerType) {
+    func createPin(itemName: String, mapPoint: MTMapPoint, markerType: MTMapPOIItemMarkerType) {
         let poiItem = MTMapPOIItem()
-        
         poiItem.itemName = "\(itemName)"
         poiItem.mapPoint = mapPoint
         poiItem.markerType = markerType
         
+        // 커스텀 뷰를 생성하여 버튼을 추가
+        let customCalloutView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+        customCalloutView.backgroundColor = .white
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+        label.textColor = .black
+        label.textAlignment = .center
+        label.text = "\(itemName)"
+        
+        customCalloutView.addSubview(label)
+        
+        // 커스텀 콜아웃 뷰 설정
+        poiItem.customCalloutBalloonView = customCalloutView
+        
         mapView.addPOIItems([poiItem])
     }
+
     // 내 위치로 트래킹
     func trackMyLocation() {
         mapView.showCurrentLocationMarker = true
         mapView.currentLocationTrackingMode = .onWithoutHeading
+    }
+    
+    func mapView(_ mapView: MTMapView!, touchedCalloutBalloonOf poiItem: MTMapPOIItem!) {
+        let ViewController2 = storyboard!.instantiateViewController(withIdentifier: "ViewController2") as! ViewController2
+        navigationController?.pushViewController(ViewController2, animated: true)
     }
     
     // Custom: 현 위치 트래킹 함수
---
+++ KakaoMap/KakaoMap/ViewController2.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+class ViewController2:UIViewController {
+    
+}
